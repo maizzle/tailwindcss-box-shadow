73d3c37f501561db0885499e5119135eae857518
Cosmin Popovici <hellocosmin@gmail.com>
2022-09-14 18:06:33 +0300

    refactor: use tailwindcss 3 plugin api

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const plugin = require('tailwindcss/plugin')
+
+const boxShadow = plugin(
+  function ({matchUtilities, theme}) {
+    matchUtilities(
+      {
+        shadow: value => ({
+          boxShadow: value
+        }),
+      },
+      { values: theme('boxShadow') }
+    )
+  },
+  {
+    corePlugins: {
+      boxShadow: false,
+    }
+  }
+)
+
+module.exports = boxShadow
